a71db3673a1a41d5d910fe10f1b0bb0fe2c41b65
J2523-Sou <j2523@matsue-ct.ac.jp>
2026-08-31 06:05:01 +0000

    チュートリアル開発完了

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,9 +1,27 @@
 class TutorialsController < ApplicationController
   def index
-    @profiles = TutorialProfile.all
+    @profiles = TutorialProfile.published
+
+    if params[:category].present?
+      @profiles = @profiles.select do |profile|
+        profile.category == params[:category]
+      end
+    end
+
+    if params[:q].present?
+      @profiles = @profiles.select do |profile|
+        profile.matches?(params[:q])
+      end
+    end
+
+    @profiles = @profiles.sort_by(&:name)
   end
 
   def debug
     @profile = TutorialProfile.find(1)
+  end
+
+  def profiles
+    @profile = TutorialProfile.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get "tutorial", to: "tutorials#index", as: :tutorial
   get "tutorial/debug", to: "tutorials#debug", as: :tutorial_debug
+  get "tutorial/profiles/:id", to: "tutorials#profiles", as: :tutorial_profile
 
     # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
     # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
